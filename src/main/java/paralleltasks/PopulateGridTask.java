8975be1943e43f0dec7f1ac61c2596c474e3b81f
georgewang0608 <yunqiw68@uw.edu>
2022-06-01 17:28:44 -0700

    part 4

--- src/main/java/paralleltasks/PopulateGridTask.java
+++ src/main/java/paralleltasks/PopulateGridTask.java
@@ -22,16 +22,54 @@ public class PopulateGridTask extends RecursiveTask<int[][]> {
     double cellWidth, cellHeight;
 
     public PopulateGridTask(CensusGroup[] censusGroups, int lo, int hi, int numRows, int numColumns, MapCorners corners, double cellWidth, double cellHeight) {
-        throw new NotYetImplementedException();
+//        throw new NotYetImplementedException();
+        this.censusGroups = censusGroups;
+        this.lo = lo;
+        this.hi = hi;
+        this.numRows = numRows;
+        this.numColumns = numColumns;
+        this.corners = corners;
+        this.cellWidth = cellWidth;
+        this.cellHeight = cellHeight;
     }
 
     @Override
     protected int[][] compute() {
-        throw new NotYetImplementedException();
+//        throw new NotYetImplementedException();
+        if (hi - lo < SEQUENTIAL_CUTOFF) {
+            return sequentialPopulateGrid();
+        }
+        int mid = lo + (hi - lo) / 2;
+        PopulateGridTask left = new PopulateGridTask(censusGroups, lo, mid, numRows, numColumns, corners, cellWidth, cellHeight);
+        PopulateGridTask right = new PopulateGridTask(censusGroups, mid, hi, numRows, numColumns, corners, cellWidth, cellHeight);
+        left.fork();
+        int[][] rightResult = right.compute();
+        int[][] leftResult = left.join();
+        POOL.invoke(new MergeGridTask(leftResult, rightResult,0, numRows, 0, numColumns));
+        return leftResult;
     }
 
     private int[][] sequentialPopulateGrid() {
-        throw new NotYetImplementedException();
+//        throw new NotYetImplementedException();
+        int[][] grid = new int[numRows][numColumns];
+
+        for (int i = lo; i < hi; i++) {
+            CensusGroup cur = censusGroups[i];
+            int colNum = (int) Math.floor((cur.longitude - corners.west) / cellWidth);
+            int rowNum = (int) Math.floor((cur.latitude - corners.south) / cellHeight);
+            if (cur.longitude == corners.east || cur.latitude == corners.north) {
+                if (cur.longitude == corners.east && cur.latitude == corners.north) {
+                    colNum -= 1;
+                    rowNum -= 1;
+                } else if (cur.longitude == corners.east) {
+                    colNum -= 1;
+                } else if (cur.latitude == corners.north) {
+                    rowNum -= 1;
+                }
+            }
+            grid[rowNum][colNum] += cur.population;
+        }
+        return grid;
     }
 }
 
